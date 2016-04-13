fa1c1bca918e6842e3d8be2d5d70303b3853dce4
GauravDhanuka <gauravdhanuka2007@gmail.com>
2016-04-14 05:28:56 +0530

    Implemented Login/Signup Modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root 'pages#home'
   get '/about' => 'pages#about' 
   get '/contact' => 'pages#contact' 

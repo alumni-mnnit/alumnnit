23b93aef18216a6799db77b8e408ad81a8278f18
GauravDhanuka <gauravdhanuka2007@gmail.com>
2016-04-16 00:23:56 +0530

    Improved Styling

--- app/assets/javascripts/ct-navbar.js
+++ app/assets/javascripts/ct-navbar.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
 $(document).scroll(function() {
    if(hasTransparent){
-        if( $(this).scrollTop() > 260 ) {
+        if( $(this).scrollTop() > 60 ) {
             if(transparent) {
                 transparent = false;
                 $('nav[role="navigation"]').removeClass('navbar-transparent');
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_filter :require_user , except: [:home, :about, :contact]
+  before_filter :require_user , except: [:home, :about]
   def home
   end
 
@@ -12,6 +12,7 @@ class PagesController < ApplicationController
   protected
 	def require_user
 	  unless current_user
+      flash[:alert] = "You need to Login/Signup to enjoy all facilities."
 	    redirect_to "/" and return
 	  end
 	end

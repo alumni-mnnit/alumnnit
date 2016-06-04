c4d8410392c87b6f9decf984bc0002da573f2259
Prashant Baisla <prashantbaisla@gmail.com>
2016-06-04 07:32:42 +0530

    Generate id card number on successful request

--- app/controllers/id_card_controller.rb
+++ app/controllers/id_card_controller.rb
@@ -5,7 +5,11 @@ class IdCardController < ApplicationController
   end
 
   def register
-    current_user.id_card_request = IdCardRequest.new status: false
-    redirect_to root_path, notice: "Successfully registered for ID Card"
+    if current_user.year_of_passing.nil? or current_user.degree.nil? or current_user.branch.nil?
+      redirect_to idcard_path, notice: "Please fill in your year of passing, degree and branch first!"
+    else
+      current_user.id_card_request = IdCardRequest.new status: false
+      redirect_to root_path, notice: "Successfully registered for ID Card"
+    end
   end
 end
--- app/models/id_card_request.rb
+++ app/models/id_card_request.rb
@@ -1,4 +1,18 @@
 class IdCardRequest < ActiveRecord::Base
   belongs_to :user
   has_one :id_card
+
+  before_update :generate_id_card_number, if: :status
+
+  def generate_id_card_number
+    year = self.user.year_of_passing.year
+    id_card_count = IdCardCount.find_by batch: year
+    if id_card_count.nil?
+      id_card_count = IdCardCount.new batch: year, count: 0
+    end
+    id_card_count.count += 1
+    id_card_number = "#{year}/#{self.user.degree}/#{self.user.branch}/#{id_card_count.count.to_s.rjust(3, '0')}"
+    self.id_card = IdCard.new id_number: id_card_number
+    id_card_count.save
+  end
 end

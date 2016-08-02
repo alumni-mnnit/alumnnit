0e74dd558b70c901a5478bc7b5b9b05308efa7a1
GauravDhanuka <gauravdhanuka2007@gmail.com>
2016-08-02 16:27:35 +0530

    Convention2016 edited

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( login-register.js login-register.css )
-Rails.application.config.assets.precompile += %w( jquery.countTo.js)
+Rails.application.config.assets.precompile += %w( login-register.js login-register.css jquery.countTo.js )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
